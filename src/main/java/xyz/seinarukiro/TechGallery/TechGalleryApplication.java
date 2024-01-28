5fbdbd597b250f6c4f826449f9fee4da74efe656
Seinarukiro2 <seinarukiro@gmail.com>
2024-01-28 01:07:14 +0100

    Initial commit

---
+++ src/main/java/xyz/seinarukiro/TechGallery/TechGalleryApplication.java
@@ -0,0 +1,13 @@
+package xyz.seinarukiro.TechGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/seinarukiro/TechGallery/TechGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.seinarukiro.TechGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
